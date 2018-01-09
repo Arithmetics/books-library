f8a07a4a9033ad5df06931e0c3112081a56a7f2f
Arithmetics <brock.m.tillotson@gmail.com>
2018-01-08 17:56:08 -0800

    changed to es6 class syntax

--- main.js
+++ main.js
@@ -27,16 +27,19 @@ formButton.addEventListener('click', function(){
   form.classList.toggle('hiding');
 })
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
+
+  toggleRead(){
+    this.read = !this.read;
+  }
 }
 
-Book.prototype.toggleRead = function(){
-  this.read = !this.read;
-}
 
 
 function addBookToLibrary(book) {
